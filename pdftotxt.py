cef380d1c3164656b58339483ece60e6e79ef5b1
pearlserpent <ismailmaj.23@gmail.com>
2023-04-19 16:58:34 +0530

    pdf to txt

--- pdftotxt.py
+++ pdftotxt.py
@@ -1,5 +1,14 @@
-from PyPDF2 import PdfReader, DocumentInformation
+from pypdf import PdfReader
 
-reader = PdfReader("list.pdf")
 
-print(reader.getDocumentInfo())
+# Converting list from pdf to txt
+
+reader = PdfReader("F_J_1.pdf")
+number_of_pages = len(reader.pages)
+
+
+with open("regular.txt", 'w') as f:
+    for i in range(number_of_pages):
+        page = reader.pages[i]
+        text = page.extract_text()
+        f.write(text)
