5bfd79a73168a12698cd77293f0862a5aeeac6c5
pearlserpent <ismailmaj.23@gmail.com>
2023-04-20 04:51:18 +0530

    edited main.py to retrieve link

--- main.py
+++ main.py
@@ -2,7 +2,25 @@ import requests
 
 # first step to automate downloading the list
 
-pdf_url = "https://main.sci.gov.in/jonew/cl/2023-04-17/M_J_1.pdf"
+import requests
+from bs4 import BeautifulSoup
+
+# Set the URL of the website to scrape
+url = 'https://main.sci.gov.in/causelist'
+
+# Send a GET request to the website
+response = requests.get(url)
+
+# Parse the HTML content of the page with Beautiful Soup
+soup = BeautifulSoup(response.content, 'html.parser')
+
+# Find the link containing 'M_J_1.pdf'
+link = soup.find('a', href=lambda href: href and 'M_J_1.pdf' in href)
+
+# Extract the URL of the link
+pdf_url = link['href']
+
+# To be modified here
 
 r = requests.get(pdf_url, stream = True)
 
