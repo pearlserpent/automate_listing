fd21848f241cab448b1ce6c8b719a23b3772189b
pearlserpent <ismailmaj.23@gmail.com>
2023-04-22 23:16:26 +0530

    edited main.py

--- main.py
+++ main.py
@@ -3,7 +3,41 @@ from bs4 import BeautifulSoup
 
 # first step to automate downloading the list
 
+date = input("DATE (YYYY-MM-DD): ")
 
+list_type = ""
+
+print("1. Miscellaneous")
+print("2. Miscellaneous Supplementary")
+print("3. Regular")
+print("4. Regular Supplementary")
+
+option = int(input("Select one option: "))
+
+if option == 1:
+    list_type = "M_J_1"
+
+elif option == 2:
+    list_type = "M_J_2"
+
+elif option == 3:
+    list_type = "F_J_1"
+
+elif option == 4:
+    list_type = "F_J_2"
+
+pdf_url = f"https://main.sci.gov.in/jonew/cl/{date}/{list_type}.pdf"
+
+r = requests.get(pdf_url, stream = True)
+
+with open("list.pdf", 'wb') as f:
+    for chunk in r.iter_content(chunk_size=1024):
+        if chunk:
+            f.write(chunk)
+
+
+
+""" 
 # Set the URL of the website to scrape
 url = 'https://main.sci.gov.in/causelist'
 
@@ -29,3 +63,4 @@ with open("list.pdf", 'wb') as f:
             f.write(chunk)
 
 
+ """
