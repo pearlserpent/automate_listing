d29250453372e4638bbdd38be7cd00d5870d8681
pearlserpent <ismailmaj.23@gmail.com>
2023-04-24 23:14:27 +0530

    automated extraction of all the available links of list, code is to be modified for downloading

--- main.py
+++ main.py
@@ -1,7 +1,48 @@
 import requests
 from bs4 import BeautifulSoup
 
-# first step to automate downloading the list
+# To automate the downloading of list
+
+# Set the URL of the website to scrape
+url = 'https://main.sci.gov.in/causelist'
+
+# Send a GET request to the website
+response = requests.get(url)
+
+# Parse the HTML content of the page with Beautiful Soup
+soup = BeautifulSoup(response.content, 'html.parser')
+
+def filter_links(href):
+    if href and ('M_J_1.pdf' in href or 'M_J_2.pdf' in href or 'F_J_1.pdf' in href or 'F_J_2.pdf' in href):
+        return True
+        
+    else:
+        return False
+    
+
+links = soup.findAll('a', href=filter_links)
+
+x = [link["href"] for link in links]
+
+ # Work need to be done below here  
+
+for i in x:
+    if i[0] == ".":
+        pass
+    else:
+        print(i) #Prints the url of the list
+
+pdf_url = f"https://main.sci.gov.in/jonew/cl/{date}/{list_type}.pdf"
+
+r = requests.get(pdf_url, stream = True)
+
+with open("list.pdf", 'wb') as f:
+    for chunk in r.iter_content(chunk_size=1024):
+        if chunk:
+            f.write(chunk)
+
+
+
 """ 
 date = input("DATE (YYYY-MM-DD): ")
 
@@ -25,43 +66,3 @@ elif option == 3:
 
 elif option == 4:
     list_type = "F_J_2" """
-
-# Set the URL of the website to scrape
-url = 'https://main.sci.gov.in/causelist'
-
-# Send a GET request to the website
-response = requests.get(url)
-
-# Parse the HTML content of the page with Beautiful Soup
-soup = BeautifulSoup(response.content, 'html.parser')
-
-def filter_links(href):
-    if href and ('M_J_1.pdf' in href or 'M_J_2.pdf' in href or 'F_J_1.pdf' in href or 'F_J_2.pdf' in href):
-        return True
-        
-    else:
-        return False
-    
-
-links = soup.findAll('a', href=filter_links)
-
-x = [link["href"] for link in links]
-
-for i in x:
-    if i[0] == ".":
-        pass
-    else:
-        print(i) #Prints the url of the list
-
-
- # Work need to be done below here  
-
-
-pdf_url = f"https://main.sci.gov.in/jonew/cl/{date}/{list_type}.pdf"
-
-r = requests.get(pdf_url, stream = True)
-
-with open("list.pdf", 'wb') as f:
-    for chunk in r.iter_content(chunk_size=1024):
-        if chunk:
-            f.write(chunk)
